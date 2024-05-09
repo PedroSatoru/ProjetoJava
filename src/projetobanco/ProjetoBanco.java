e84af25d50b17a59462942d03ec6e4224ee54e97
PedroSatoru <pedrohsatoru@gmail.com>
2024-05-09 20:07:23 -0300

    Teste

---
+++ src/projetobanco/ProjetoBanco.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetobanco;
+
+import projetobanco.View.JanelaLogin;
+
+/**
+ *
+ * @author Pedro Satoru
+ */
+public class ProjetoBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaLogin Jl = new JanelaLogin();
+        Jl.setVisible(true);
+    }
+    
+}

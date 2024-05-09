ad640cb659a4d4cb2549a8bcf0a94c783fe38214
PedroSatoru <pedrohsatoru@gmail.com>
2024-05-09 20:10:31 -0300

    Salvamento de todos arquivos

---
+++ src/projetobanco/Controller/ControllerLogin.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetobanco.Controller;
+
+
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import projetobanco.DAO.Conexao;
+import projetobanco.DAO.UsuarioDAO;
+import projetobanco.Model.Usuario;
+import projetobanco.View.JanelaLogin;
+
+public class ControllerLogin {
+    private JanelaLogin view;
+
+    public ControllerLogin(JanelaLogin view) {
+        this.view = view;
+    }
+    
+    public void loginUsuario(){
+        Usuario usuario = new Usuario(null, view.getTxtCpf().getText(),
+                                            view.getTxtSenha().getText());
+        
+        Conexao conexao = new Conexao();
+        try{
+            Connection conn = conexao.getConnection();
+            UsuarioDAO dao = new UsuarioDAO(conn);
+            ResultSet res = dao.consultar(usuario);
+            if (res.next()){
+                JOptionPane.showMessageDialog(view, "Login feito!", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(view, "Login não efetuado!");
+            }
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(view, "Erro de conexão!");
+        }
+    }
+    
+}
---
+++ src/projetobanco/DAO/UsuarioDAO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetobanco.DAO;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+
+import projetobanco.Model.Usuario;
+public class UsuarioDAO {
+    private Connection conn;
+
+    public UsuarioDAO(Connection conn) {
+        this.conn = conn;
+    }
+    
+    public ResultSet consultar (Usuario usuario) throws SQLException {
+        String sql = "select * from Usuarios where CPF = ? and Senha = ?";
+
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setString(1, usuario.getCpf());
+        statement.setString(2, usuario.getSenha());
+        statement.execute();
+        ResultSet resultado = statement.getResultSet();
+        return resultado;
+    }
+       
+}
+
